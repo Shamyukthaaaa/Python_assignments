474dfa95bc9cc54c79daee13f23b68dadc6cc553
Shamyukthaaaa <86068493+Shamyukthaaaa@users.noreply.github.com>
2021-07-11 17:15:55 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,59 @@
+#Strings To Be Implemented Methods
+s="Python programming"
+h=" 1234 "
+i="good"
+#index method
+s.index("n")
+print(s)
+s.index(" ")
+#uppercase method
+s.upper()
+print(s)
+#lowercase method
+print(s.lower())
+#isuppercase method
+print(s.isupper())
+#is lowercase method
+print(s.islower())
+#swapcase method
+print(s.swapcase())
+#count method
+print(s.count("m"))
+#isnumeric method
+print(h.isnumeric())
+#isalnum
+print(h.isalnum())
+#isdigit
+print(h.isdigit())
+#isdecimal
+print(h.isdecimal())
+#join method
+s.join(h)
+print(s)
+#rjust method
+print(h.rjust(5))
+#ljust method
+print(s.ljust(10))
+#strip method
+print(h.strip())
+#length method
+print(len(s))
+#find method
+print(s.find("o"))
+#startswith method
+print(s.startswith("Pyth"))
+#endswith method
+print(s.endswith("ng"))
+#replace method
+print(s.replace("P","H"))
+#capitalize method
+print(i.capitalize())
+#title method
+print(s.title())
+#isprintable method
+print(i.isprintable())
+#split method
+print(s.isprintable())
+#delete method
+del s
+print(s)
